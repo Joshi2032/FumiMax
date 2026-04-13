51d2731dc91484e19416b36075b1c81805a5181c
Josue Isai Salazar Castro <joshi.jc2032@gmail.com>
2026-04-13 00:07:26 -0600

    Landing page profesional Angular

---
+++ src/app/pages/landing.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.html',
+  styleUrls: ['./landing.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LandingComponent {
+  menuOpen = false;
+}
