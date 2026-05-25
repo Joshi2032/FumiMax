91eb7fe7d76831ddd423d26f17e84b31c98f6183
Josue Isai Salazar Castro <joshi.jc2032@gmail.com>
2026-05-24 19:18:03 -0600

    adaptacion 4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,38 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/hero/hero').then(m => m.Hero) },
-  { path: 'servicios', loadComponent: () => import('./pages/servicios/servicios').then(m => m.Servicios) },
-  { path: 'nosotros', loadComponent: () => import('./pages/nosotros/nosotros').then(m => m.Nosotros) },
-  { path: 'contacto', loadComponent: () => import('./pages/contacto/contacto').then(m => m.Contacto) },
-  { path: 'cobertura', loadComponent: () => import('./pages/cobertura/cobertura').then(m => m.Cobertura) }
+	{
+		path: '',
+		title: 'MAXIFUM DEL BAJÍO | Control Profesional de Plagas',
+		loadComponent: () => import('./pages/landing').then(m => m.LandingComponent)
+	},
+	{
+		path: 'servicios',
+		title: 'Nuestros Servicios | MAXIFUM',
+		loadComponent: () => import('./pages/servicios/servicios').then(m => m.Servicios)
+	},
+	{
+		path: 'nosotros',
+		title: 'Quiénes Somos | MAXIFUM',
+		loadComponent: () => import('./pages/nosotros/nosotros').then(m => m.Nosotros)
+	},
+	{
+		path: 'cobertura',
+		title: 'Zonas de Cobertura | MAXIFUM',
+		loadComponent: () => import('./pages/cobertura/cobertura').then(m => m.Cobertura)
+	},
+	{
+		path: 'faq',
+		title: 'Preguntas Frecuentes | MAXIFUM',
+		loadComponent: () => import('./pages/faq/faq').then(m => m.Faq)
+	},
+	{
+		path: 'contacto',
+		title: 'Contacto y Cotización | MAXIFUM',
+		loadComponent: () => import('./pages/contacto/contacto').then(m => m.Contacto)
+	},
+	{
+		path: '**',
+		redirectTo: ''
+	}
 ];
